915892340df15efce4191eb41319d63a2ced582d
flying-pig <315960451@qq.com>
2024-05-08 13:14:04 +0800

    Update Messages.py
    explain @staticmethod

--- app/Messages.py
+++ app/Messages.py
@@ -2,7 +2,7 @@
 # @yasinkuyu
 
 class Messages():
-    
+    ## use @staticmethod , so the funtion can be called without instantiating the class.
     @staticmethod
     def get(msg):
         print('Message : ' + msg)
